a94759f0a4dd72102aab91f77eaf5f55cef1ca09
vanshuhassija <vanshuhassija@gmail.com>
2022-01-14 18:04:25 +0530

    Allow All Origins

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const mysql = require("mysql");
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: "*",
     methods: ["GET", "POST", "PATCH"],
   },
 });
